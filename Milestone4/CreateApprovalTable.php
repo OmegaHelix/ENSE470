ddd486268c70c2a7483b208819ed8c406aa33d7f
oscar666666 <oscar88feichang6@gmail.com>
2018-02-28 16:31:07 -0600

    Add files via upload

---
+++ Milestone4/CreateApprovalTable.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "HELLDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+$sql = "CREATE TABLE Approval (
+userid INT(6) NOT NULL, 
+softwareid INT(6) NOT NULL,
+FOREIGN KEY (userid) REFERENCES Login(id),
+FOREIGN KEY (softwareid) REFERENCES Software(id),
+PRIMARY KEY(userid, softwareid)
+
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
