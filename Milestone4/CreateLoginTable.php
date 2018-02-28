8007e2fc4b92817286f8b28c643cf62f12cb15bb
oscar666666 <oscar88feichang6@gmail.com>
2018-02-28 16:01:45 -0600

    Add files via upload

---
+++ Milestone4/CreateLoginTable.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "HELLDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+$sql = "CREATE TABLE Software (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(50) NOT NULL,
+password INT NOT NULL,
+access VARCHAR(20),
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
